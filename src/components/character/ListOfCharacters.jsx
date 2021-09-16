ecdfb4bf92f7210f95791698a0ebe8a325788632
BenFrancka <benfrancka@gmail.com>
2021-09-15 17:07:55 -0700

    page is working as it should

--- src/components/character/ListOfCharacters.jsx
+++ src/components/character/ListOfCharacters.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CharacterListItem from '../characters/CharacterListItem';
+import CharacterListItem from '../character/CharacterListItem';
 import { Link } from 'react-router-dom';
 import { useCharacter } from '../../hooks/useCharacter';
 
