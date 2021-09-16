e9e374c43fc02b7da9cbb502657c2da5a5b2b54d
BenFrancka <benfrancka@gmail.com>
2021-09-15 17:30:48 -0700

    adds detail test, all tests are passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -5,6 +5,7 @@ import { setupServer } from 'msw/node';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import listData from '../../fixtures/listData.json';
+import detailData from '../../fixtures/detailData.json';
 
 const server = setupServer(
   rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
@@ -26,6 +27,32 @@ describe('Rick and Morty info App', () => {
     screen.getAllByAltText('loading spinner');
 
     const ul = await screen.findByRole('list', { name: 'character-list' });
+    expect(ul).not.toBeEmptyDOMElement();
+    expect(container).toMatchSnapshot();
+  });
+});
+
+const serverDetail = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character/:id', (req, res, ctx) => {
+    return res(ctx.json(detailData));
+  })
+);
+
+describe('Rick and Morty info App', () => {
+  beforeAll(() => serverDetail.listen());
+  afterAll(() => serverDetail.close());
+
+  // eslint-disable-next-line max-len
+  it('displays a detail view of a single Rick and Morty Character', async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/2']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    screen.getAllByAltText('loading spinner');
+
+    const ul = await screen.findByRole('list', { name: 'character details' });
     expect(ul).not.toBeEmptyDOMElement();
     expect(container).toMatchSnapshot();
   });
